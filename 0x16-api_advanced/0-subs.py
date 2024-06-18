6f5b047688d9fdf6f3d8eb8431b52f53af479217
Cappu123 <cappyalemayehu12@gmail.com>
2024-06-18 03:47:32 -0700

    results number of subscribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,7 +10,8 @@ def number_of_subscribers(subreddit):
     subscribers for a given subreddit"""
     url = 'https://www.reddit.com/subreddit/{}/about.json'.format(subreddit)
     headers = {'User-Agent': 'Cappu Agent 1.0'}
-    response = requests.get(url, headers=headers)
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
     if response.status_code == 200:
         data = response.json()
         return data.get('data').get('subscribers')
