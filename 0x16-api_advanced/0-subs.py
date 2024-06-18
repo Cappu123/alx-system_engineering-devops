916261b79d71490c4fd0ead8fbf48a86d60f4784
Cappu123 <cappyalemayehu12@gmail.com>
2024-06-18 03:45:56 -0700

    results number of subscribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,29 +1,18 @@
 #!/usr/bin/python3
+"""function that queries the Reddit API and returns the number of subscribers
+for a given subreddit"""
 
-"""
-for returning number of subscribers for the given subreddit
-"""
-
-"""Import the python request module to fetch http requests
-"""
 import requests
 
-def number_of_subscribers(subreddit):
-    """A function that queiries the Reddit API and
-    returns the number of subscribers
-    """
-    url = f"https://reddit.com/{subreddit}/about.json()"
-    """Define the header including User-Agent
-    as per the Reddit API access rules
-    """
-    headers = {'User-Agent': 'cappu reddit 1.0'}
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
 
-        if response.status_code == 200:
-            data = response.json()
-            return data.get("data").get("subscribers")
-        else:
-            return 0
-    except requests.RequestException:
+def number_of_subscribers(subreddit):
+    """function that queries the Reddit API and returns the number of
+    subscribers for a given subreddit"""
+    url = 'https://www.reddit.com/subreddit/{}/about.json'.format(subreddit)
+    headers = {'User-Agent': 'Cappu Agent 1.0'}
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        data = response.json()
+        return data.get('data').get('subscribers')
+    else:
         return 0
