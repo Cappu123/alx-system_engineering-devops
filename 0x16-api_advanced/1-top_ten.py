f9574d46b18506ca4a502a73414186aee5246a18
Cappu123 <cappyalemayehu12@gmail.com>
2024-06-19 01:08:33 -0700

    change

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -12,18 +12,21 @@ def top_ten(subreddit):
     url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
 
     headers = {"User-Agent": "Cappu Agent"}
-    response = requests.get(url, headers=headers, allow_redirects=False)
 
     try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
         if response.status_code == 200:
             data = response.json()
             posts = data.get('data', {}).get('children', [])
 
-            for post in posts:
-                title = post.get('data', {}).get('title', {})
-                if title:
-                    print(title)
+            if posts:
+                for post in posts:
+                    title = post.get('data', {}).get('title', "")
+                    if title:
+                        print(title)
+            else:
+                print('None')
         else:
-            print('None')
+            print("None")
     except Exception as e:
         print('None')
